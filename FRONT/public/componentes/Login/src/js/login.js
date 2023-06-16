1cd62da033ded39ad13a49897e6258fc1975c7ea
Travi995 <joseantoniotravieso259@gmail.com>
2023-06-16 16:00:50 -0400

    chore (JTB/Front): agrego del boton enviar al componente login

--- FRONT/public/componentes/Login/src/js/login.js
+++ FRONT/public/componentes/Login/src/js/login.js
@@ -5,12 +5,14 @@ const mostrarForm = () => {
     elementos.forEach(element => {
         element.addEventListener('click', () => {
             document.getElementById('formulario').style.display = 'block'
-            document.getElementById('imgSesion').style.marginLeft = '5px'
+            document.getElementById('imgSesion').style.marginLeft = '53px'
         })   
     });
     
-    
-
+//    esto para eliminar el layout de login en cuestion  
+    document.getElementById('enviar').addEventListener('click', () => {
+        document.getElementById('login').style.display = 'none'
+    })
 }
 
 
