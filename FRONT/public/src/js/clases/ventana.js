9f5543891f59353cd70afb043dfaddd81e28f642
Travi995 <joseantoniotravieso259@gmail.com>
2023-07-07 21:24:42 -0400

    chore(JTB/Front):funcionalidad de arrastrar la ventana completada

--- FRONT/public/src/js/clases/ventana.js
+++ FRONT/public/src/js/clases/ventana.js
@@ -1,9 +1,10 @@
 let errorVentana = ''
+let dragTerminado = ''
 
-class ventana{
+class ventana {
     constructor() {
         let ventana = HTMLDivElement
-        let head    = HTMLDivElement
+        let head = HTMLDivElement
         this.compoGraph()
         this.cargarEventos()
     }
@@ -11,58 +12,108 @@ class ventana{
     compoGraph() {
         let elementoFijo = document.createElement('div')
 
-        this.ventana= document.createElement('div')
-        this.head   = document.createElement('div')
+        this.ventana = document.createElement('div')
+        this.head = document.createElement('div')
 
         this.ventana.classList.add('ventana')
         this.head.classList.add('headVentana')
         elementoFijo.classList.add('iconFijoVentana')
 
-        
-        elementoFijo.appendChild(new iconoHeadVentana('./src/iconos/1904666-calculate-close-delete-hide-minimize-minus-remove_122516.ico',  'minimizar').icono)
+        this.ventana.setAttribute('draggable', 'true')
+
+        elementoFijo.appendChild(new iconoHeadVentana('./src/iconos/1904666-calculate-close-delete-hide-minimize-minus-remove_122516.ico', 'minimizar').icono)
         elementoFijo.appendChild(new iconoHeadVentana('./src/iconos/maximize_icon_143740.ico', 'maximizar').icono)
         elementoFijo.appendChild(new iconoHeadVentana('./src/iconos/cross-2_90852.ico', 'cerrar').icono)
-        
-        
-        
+
+
+
         this.head.appendChild(elementoFijo)
         this.ventana.appendChild(this.head)
-        
+
     }
 
     cargarEventos() {
         let botonesVentana = this.head.children
-        botonesVentana = Array.from(botonesVentana[0].children)
         
+
+
+        botonesVentana = Array.from(botonesVentana[0].children)
+
         botonesVentana.forEach(element => {
             element.addEventListener('click', (arg) => {
                 this.manejarEventos(arg)
             })
         });
+
+        /*drag and drop de la ventana*/
+        let posIniX 
+        let posIniY
+        let posFinX
+        let posFinY
+
+        this.ventana.addEventListener('dragstart', (event) => {
+            
+            posIniX = event.offsetX
+            posIniY = event.offsetY
+                  
+        })
+
+        this.ventana.addEventListener('dragover', (event) => {
+            event.preventDefault()
+            //  console.log('dragover se esta ejecutando')
+        })
+        this.ventana.addEventListener('dragend', (event) => {
+            event.preventDefault()
+
+            console.log('dragend terminado')
+           
+            posFinX = event.clientX
+            posFinY = event.clientY
+            
+            let coordenadaX = posFinX - posIniX
+            let coordenadaY = posFinY - posIniY
+            if (coordenadaX < 0 ) {
+                coordenadaX = coordenadaX* -1
+            }else if (coordenadaY < 0 ) {
+                coordenadaY = coordenadaY* -1
+            }
+            console.log(event)
+            console.log(coordenadaX)
+            console.log(coordenadaY)
+
+            this.ventana.style.left = `${coordenadaX}px`;
+            this.ventana.style.top = `${coordenadaY}px` ;
+
+        })
+
+
+
+
+
     }
 
     manejarEventos(arg) {
         let item = arg.target
-        
-        if (item.tagName == 'IMG')       item = item.parentNode
+
+        if (item.tagName == 'IMG') item = item.parentNode
         else if (item.tagName == 'SPAN') item = item.parentNode
 
         switch (item.getAttribute('iconId')) {
-            
+
             case 'minimizar':
-                alert('toco uno de los tres botones')
+                this.ventana.classList.add('minimizar')
                 break;
-            
-            
+
+
             case 'maximizar':
-                    this.ventana.classList.add('minimizar')
+                alert('deshabilitado')
                 break;
-            
+
             case 'cerrar':
-                    this.ventana.remove()
+                this.ventana.remove()
                 break;
-            
-            
+
+
 
 
         }
